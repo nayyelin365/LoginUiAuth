5c9bbea2969dbcce34cc7af589e2a2343540ac1b
Nay Ye Lin <nayye5154@gamil. om>
2019-11-29 12:36:35 +0630

    NYL brunch

---
+++ resources/views/TestA.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TestA</title>
+</head>
+<body>
+
+<h1>TestA</h1>
+</body>
+</html>
---
+++ resources/views/bruchNYL.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NYL brunch</title>
+</head>
+<body>
+
+<h1>NYL</h1>
+</body>
+</html>
